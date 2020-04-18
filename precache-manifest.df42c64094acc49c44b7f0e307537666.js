a66e818c9ffa315a67953efcc6864655816760ae
唐词伟 <tangciwei@bytedance.com>
2020-04-18 21:00:45 +0800

    feat: init
    Change-Id: Icfc02225860eecf4230c75374142e181801789b6

---
+++ precache-manifest.df42c64094acc49c44b7f0e307537666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c0e86815225c94b8618d6bea8d0706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0db2f785af0a051323",
+    "url": "/static/css/main.8a775c3c.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "db0db2f785af0a051323",
+    "url": "/static/js/main.83f805f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
